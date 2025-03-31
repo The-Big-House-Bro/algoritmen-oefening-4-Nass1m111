385cc90754065710f476e8e1030839a7bb7598ee
Nassim Hamdi <nassimhamdi1@outlook.com>
2025-03-31 20:11:47 +0200

    Opdracht: Algoritmen Oefening 4 - Nassim Hamdi - 6ECWI

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,14 @@
-def countTargetPairs():
+def countTargetPairs(nums, target):
+    aantalPairs = 0
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] < target:
+                aantalPairs = aantalPairs + 1
+    return aantalPairs
+
+# 1e voorbeeld
+print(countTargetPairs([-1, 1, 2, 3, 1], 2))
+
+
